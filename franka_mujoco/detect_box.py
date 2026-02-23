1708bd7d750fa3995ec5b5de9bcaf4885c713b8b
Faiz Aladin <87955636+faizaladin@users.noreply.github.com>
2026-02-23 10:40:19 -0800

    Update detect_box.py

--- franka_mujoco/detect_box.py
+++ franka_mujoco/detect_box.py
@@ -19,43 +19,29 @@ class BoxDetector(Node):
         self.get_logger().info("Looking for the Green Box (Rotated View)...")
 
     def image_callback(self, msg):
-        # 1. Convert ROS Image to OpenCV
         cv_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
         
-        # --- ROTATE THE FEED ---
-        # Rotate 90 degrees Counter-Clockwise
         cv_image = cv2.rotate(cv_image, cv2.ROTATE_90_COUNTERCLOCKWISE)
-        # -----------------------
-        
-        # 2. Convert to HSV
+    
         hsv = cv2.cvtColor(cv_image, cv2.COLOR_BGR2HSV)
         
-        # 3. Define Green Range
         lower_green = np.array([35, 100, 100])
         upper_green = np.array([85, 255, 255])
         
-        # 4. Create Mask
         mask = cv2.inRange(hsv, lower_green, upper_green)
         
-        # 5. Find Contours
         contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
         
         if contours:
             c = max(contours, key=cv2.contourArea)
             
             if cv2.contourArea(c) > 100:
-                # Get bounding box
                 x, y, w, h = cv2.boundingRect(c)
-                
-                # Draw Rectangle
                 cv2.rectangle(cv_image, (x, y), (x + w, y + h), (0, 0, 255), 2)
-                
-                # Draw Center Dot
                 cx = x + w // 2
                 cy = y + h // 2
                 cv2.circle(cv_image, (cx, cy), 5, (0, 0, 255), -1)
 
-        # 6. Show the result
         cv2.imshow("Original Feed", cv_image)
         cv2.imshow("Green Mask", mask)
         cv2.waitKey(1)
